01613d1427ad38a5251d150774662922f6797196
nctygum <gum.sahundi@gmail.com>
2021-10-28 17:06:06 +0700

    Project Pertama Git dan Github dengan CodeIgniter

---
+++ application/views/beranda.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta chars="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale =1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2><center>Hallo saya Gugum Gumilar S Handoko (12200315)</center></h2><tr>
+        <center>Ini Adalah Halaman Utama dari project CodeIgniter saya</center>
+</body>
+</html>
